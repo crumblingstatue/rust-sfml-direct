0f4273b3a63a265b663632583d3d864d5260df45
crumblingstatue <radiantstatue@gmail.com>
2018-10-22 17:21:44 +0200

    Whoops, only apply #[allow] for the `raw` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-#![allow(
+#[allow(
     non_upper_case_globals,
     non_camel_case_types,
     non_snake_case,
     clippy::all
 )]
-
 mod raw {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
